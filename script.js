4b848401dfeb721e4dfa01723509b87690d60055
Jason Hawkins <Jason_Hawkins@live.com>
2020-09-10 06:58:16 -0600

    project complete

--- script.js
+++ script.js
@@ -19,6 +19,13 @@ function emptyList() {
     alert("Clear list button clicked!");
 }
 
+var saveButton = document.getElementById("save-button");
+saveButton.addEventListener("click", saveList);
+
+function saveList() {
+    alert("Save button clicked!");
+}
+
 var toDoEntryBox = document.getElementById("todo-entry-box");
 var toDoList = document.getElementById("todo-list");
 
@@ -64,3 +71,27 @@ function emptyList() {
     }
 }
 
+function saveList() {
+    var toDos = [];
+    for (var i = 0; i < toDoList.children.length; i++) {
+    var toDo = toDoList.children.item(i);
+    var toDoInfo = {
+    "task": toDo.innerText,
+    "completed": toDo.classList.contains("completed")
+    };
+    toDos.push(toDoInfo);
+    }
+    localStorage.setItem("toDos", JSON.stringify(toDos));
+    }
+
+function loadList() {
+    if (localStorage.getItem("toDos") != null) {
+	var toDos = JSON.parse(localStorage.getItem("toDos"));
+	for (var i = 0; i < toDos.length; i++) {
+	   var toDo = toDos[i];
+	   newToDoItem(toDo.task, toDo.completed);
+	   }
+    }
+}
+
+loadList();
